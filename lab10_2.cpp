61b5bdcd7afd6ac485f42218a97b6b6d51099b6e
krapaomukrob <96363202+Pannathornnn@users.noreply.github.com>
2025-12-26 22:58:55 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,8 +9,16 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline ;
+	dest << "-------------------- BOOM ---------------------" << endl ;
+	cout << "-------------------- BOOM ---------------------" << endl ;
+	while(getline(source , textline))
+	{
+		cout << textline << endl ;
+		dest << textline << endl;
+	}
+	cout << "-------------------- HA!! ---------------------" ;
+	dest << "-------------------- HA!! ---------------------" ;
 
     source.close();
     dest.close();
